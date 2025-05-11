0cc48f23040f2c21ce221f62ffed77f2b0528a5b
YukioE <93155932+YukioE@users.noreply.github.com>
2025-05-11 15:27:11 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+app.use(express.json());
+
+app.get('/', (_, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`\nhttps://localhost:${port},\n${new Date().toLocaleString()}`);
+});
