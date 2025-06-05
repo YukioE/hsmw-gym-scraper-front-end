75a11ff301b2bbf7c3a98002168fe2f77f050095
YukioE <93155932+YukioE@users.noreply.github.com>
2025-06-06 00:50:24 +0200

    docker init
    .

--- src/browser.ts
+++ src/browser.ts
@@ -6,6 +6,7 @@ export const getBrowser = async (): Promise<Browser> => {
     if (!browserInstance) {
         browserInstance = await puppeteer.launch({
             headless: true,
+            executablePath: '/usr/bin/google-chrome',
             args: ["--disable-setuid-sandbox"],
             ignoreHTTPSErrors: true,
         });
